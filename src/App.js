e9f4c2cd950eeaaaca5774c7ded31b9d0c10061b
Kanav Khajuria <kanav.khajuria@proptiger.com>
2020-07-11 21:51:03 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {
+  View
+} from 'react-native';
+
+class App extends React.PureComponent {
+  render() {
+    return <View style={{ backgroundColor: 'steelblue', flex: 1}}/>
+  }
+}
+export default App;
